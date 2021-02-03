9a1aa5542ab0d027deb7854b56e1e14e9d2e7dfa
Jasmine <jasylop@gmail.com>
2021-02-03 12:49:05 -0800

    spring react oauth tutorial part 3

---
+++ src/common/AppHeader.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import './AppHeader.css';
+
+class AppHeader extends Component {
+    render() {
+        return (
+            <header className="app-header">
+                <div className="container">
+                    <div className="app-branding">
+                        <Link to="/" className="app-title">Recipe Stash</Link>
+                    </div>
+                    <div className="app-options">
+                        <nav className="app-nav">
+                                { this.props.authenticated ? (
+                                    <ul>
+                                        <li>
+                                            <NavLink to="/recipes">Recipes</NavLink>
+                                        </li>
+                                        <li>
+                                            <NavLink to="/profile">Profile</NavLink>
+                                        </li>
+                                        <li>
+                                            <button onClick={this.props.onLogout}>Logout</button>
+                                        </li>
+                                    </ul>
+                                ): (
+                                    <ul>
+                                        <li>
+                                            <NavLink to="/login">Login</NavLink>        
+                                        </li>
+                                        <li>
+                                            <NavLink to="/signup">Signup</NavLink>        
+                                        </li>
+                                    </ul>
+                                )}
+                        </nav>
+                    </div>
+                </div>
+            </header>
+        )
+    }
+}
+
+export default AppHeader;
---
+++ src/recipes/Recipes.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Recipes = (props) => {
+  console.log(props);
+  return (
+    <h1>RECIPES</h1>
+  )
+
+}
+
+export default Recipes;
